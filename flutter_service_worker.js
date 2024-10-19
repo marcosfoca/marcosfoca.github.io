eb0b4955df4ad298bcc9d1a90bb0a20683b814e7
marcosfoca <marcosfortis@gmail.com>
2024-10-19 15:47:40 +0200

    replaced files to fix website not opening

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "45e27f547ce869b86d2f13e78a9fb1d1
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.PNG": "90a14113d61decd44cdb19a09b420971",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "f20711fc24a8f3be16bab5126c810ed3",
+"flutter_bootstrap.js": "8842ef4297d5c5edb6dc1604ab11df8d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
